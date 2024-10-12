1f7da324dad81e1361c4663681f7ffc6c0316898
Malena <malenasantos774@gmail.com>
2024-10-12 13:14:34 -0300

    Update jogo_da_velha.py

--- modulo/jogo_da_velha.py
+++ modulo/jogo_da_velha.py
@@ -1,4 +1,5 @@
 # trabalho 1
+# importando bibliotecas
 import random 
 import numpy as np
 
@@ -64,29 +65,90 @@ class JogoVelha:
             return self.jogador_maq 
         
 class Tabuleiro:
-    def __init__(self):
+"""
+Classe que representa o tabuleiro
+"""
+    def __init__(self)-> None:
+        """
+        Inicializa um tabuleiro vazio
+
+        Returns:
+        None: a função retorna nada
+        """
         self.casas = np.full((3,3), '.')
         
     def marcar_tabuleiro(self, linha: int, coluna: int, simbolo: str) -> None:
+        """
+        Marca uma posição no tabuleiro com o símbolo do jogador
+
+        Args:
+        coluna(int): número que representa a cordenada da coluna do tabuleiro
+        linha(int): número que representa a cordenada da linha do tabuleiro
+        simbolo(str): caractere que representa o simbolo do jogador 
+
+        Returns:
+        None: a função retorna nada
+        """
         if self.casas[linha][coluna] == '.':
             self.casas[linha][coluna] = simbolo  
             
-    def imprimir_tabuleiro(self):
+    def imprimir_tabuleiro(self)->None:
+        """
+        Apenas imprime o tabuleiro atualizado
+
+        Returns:
+        None: a função retorna nada
+        """
         print('\n')
         print(self.casas)
 1
 class Jogador:
+    """
+    Classe que representa o jogador
+    """
     def __init__(self, simbolo: str) -> None:
+        """
+        Inicializa um jogador
+
+        Args:
+        simbolo(str): caractere que representa o simbolo do jogador
+
+        Returns:
+        None: a função retorna nada
+        """
         self.simbolo = simbolo
     
     def fazer_jogada(self) -> None:
+        """
+        Função que será modelada nas subclasses
+
+        Returns: a função retorna nada
+        """
         pass
 
 class JogadorHumano(Jogador):
+    """
+    Suclasse de Jogador, representa o jogador humano
+    """
     def __init__(self, simbolo: str) -> None:
         super().__init__(simbolo)
         
     def fazer_jogada(self) -> tuple:
+        """
+        Pede a cordernada da linha e coluna da jogada
+
+        Returns:
+        tuple:     def fazer_jogada(self) -> tuple:
+        """
+        Pede a cordernada da linha e coluna da jogada
+
+        Returns:
+        tuple: a função retorna uma tuple com a cordenada da linha e coluna nessa ordem
+        """
+        linha = int(input('Indique a linha da sua jogada (0,1,2): '))
+        coluna = int(input('Indique a coluna da sua jogada (0,1,2): '))
+        return linha, coluna
+        """
         linha = int(input('Indique a linha da sua jogada (0,1,2): '))
         coluna = int(input('Indique a coluna da sua jogada (0,1,2): '))
         return linha, coluna
@@ -97,6 +159,14 @@ class JogadorComputador(Jogador):
         self.tabuleiro = tabuleiro
         
     def fazer_jogada(self) -> tuple:
+        """
+        Laço que gerar aleatoriamente duas cordenadas para linha e coluna e retorna uma 
+        tupla se ambas forem validas
+        
+        Returns
+        tuple: a função retorna uma tuple com a cordenada da linha e coluna nessa ordem
+        """
+       
         while True:
             linha = random.randint(0, 2)
             coluna = random.randint(0, 2)
