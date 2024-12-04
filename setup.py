128bd4ae9b14d8a37b354505bcb00ffd126dae53
Malena <malenasantos774@gmail.com>
2024-12-04 10:31:32 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="finance_backend", 
+    name="Jogo_da_Velha", 
     version="0.1.0",  
-    description="Backend de um app de finanças",
+    description="simulação de um jogo da velha",
     long_description=open("README.md").read(),
     license="MIT", 
-    author="Malena Milani dos Santos, Wangleycia Lorena Silva de Oliveira",
+    author="Malena Milani dos Santos",
     python_requires=">=3.8", 
-    install_requires=[], 
-    url="https://github.com/Project-OOP-2/Project-2.git",
+    install_requires=['numpy','pandas'], 
+    url= "https://github.com/Malena-yyy/modulo.git" ,
     packages=find_packages(), 
 
 )
